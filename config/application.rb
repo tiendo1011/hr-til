ce71dc006969202f4ce952b15f3363b2b4349ff1
tiendo1011 <tiendo1011@gmail.com>
2019-09-15 14:52:27 +0700

    Use db/schema.rb instead

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ Bundler.require(*Rails.groups)
 
 module HrTil
   class Application < Rails::Application
-    config.active_record.schema_format = :sql
+    config.active_record.schema_format = :ruby
     config.autoload_paths << Rails.root.join('lib')
     config.time_zone = 'Eastern Time (US & Canada)'
 
